ddb86a0bb2a54847ba8cd9510dbb4a5ff30e708e
Sithum Suraweera <130219154+suraweera-AGSS@users.noreply.github.com>
2023-12-27 15:21:42 +0530

    Cus_Payment page

---
+++ Customer_Payment.java
@@ -0,0 +1,58 @@
+package cus_pay;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cus_reg.DBUtil;
+
+@WebServlet("/Customer_Payment")
+@MultipartConfig
+public class Customer_Payment extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String holderName = request.getParameter("cardHolder");
+        String paymentMethod = request.getParameter("payment_method");
+        String cardNumber = request.getParameter("cardNumber");
+        String payDate = request.getParameter("payDate");
+        String amount = request.getParameter("amount");
+
+        Connection connection = null;
+
+        try {
+            connection = DBUtil.getConnection();
+            PreparedStatement stmt;
+            String query = "INSERT INTO payment (holderName, paymentMethod, cardNumber, payDate, amount) VALUES (?, ?, ?, ?, ?)";
+            stmt = connection.prepareStatement(query);
+            stmt.setString(1, holderName);
+            stmt.setString(2, paymentMethod);
+            stmt.setString(3, cardNumber);
+            stmt.setString(4, payDate);
+            stmt.setString(5, amount);
+            int row = stmt.executeUpdate();
+
+            if (row > 0) {
+                System.out.println("Payment details added into database successfully...");
+                response.sendRedirect("payment_details.jsp?cardHolder=" + holderName +
+                        "&payment_method=" + paymentMethod +
+                        "&cardNumber=" + cardNumber +
+                        "&payDate=" + payDate +
+                        "&amount=" + amount);
+            } else {
+                System.out.println("Failed to add payment details into Database...");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.closeConnection(connection);
+        }
+    }
+}
---
+++ DBUtil.java
@@ -0,0 +1,36 @@
+package cus_pay;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+    private static final String url = "jdbc:mysql://localhost:3306/trains";
+    private static final String username = "root";
+    private static final String password = "12345";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public static void closeResources(Connection connection, PreparedStatement preparedStatement) {
+        try {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+	public static void closeConnection(Connection connection) {
+		// TODO Auto-generated method stub
+		
+	}
+}
